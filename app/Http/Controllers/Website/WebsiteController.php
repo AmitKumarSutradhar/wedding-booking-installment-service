d2148b1b5228864c36fc0a899f532b7d60ba153e
Amit Kumar Sutradhar <dev.amitkumarsutradhar@gmail.com>
2025-01-21 19:02:01 +0600

    first commit

---
+++ app/Http/Controllers/Website/WebsiteController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Website;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class WebsiteController extends Controller
+{
+    public function adminLogin(){
+        return view('auth.admin-login');
+    }
+
+    public function sellerLogin(){
+        return view('auth.seller-login');
+    }
+
+    public function sellerRegister(){
+        return view('auth.seller-register');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Website\WebsiteController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/admin/auth/login', [WebsiteController::class, 'adminLogin'])->name('admin.login');
+Route::get('/seller/auth/login', [WebsiteController::class, 'sellerLogin'])->name('seller.login');
+Route::get('/seller/auth/register', [WebsiteController::class, 'sellerRegister'])->name('seller.register');
+
+Route::get('/dashboard', function () {
+    return view('admin-views.dashboard.index');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
