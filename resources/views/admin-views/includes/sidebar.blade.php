4e42f5eb5a014c4a27c1a8dc741d1eae6fdb13a6
Amit Kumar Sutradhar <dev.amitkumarsutradhar@gmail.com>
2025-01-22 23:41:37 +0600

    Add admin side menu items

--- resources/views/admin-views/includes/sidebar.blade.php
+++ resources/views/admin-views/includes/sidebar.blade.php
@@ -86,26 +86,54 @@
                 </ul>
             </div>
         </li>
-        <li class="nav-item nav-category">User management</li>
+
+        <li class="nav-item nav-category">Seller management</li>
         <li class="nav-item">
-            <a class="nav-link" data-bs-toggle="collapse" href="#icons" aria-expanded="false" aria-controls="icons">
-                <i class="menu-icon mdi mdi-layers-outline"></i>
-                <span class="menu-title">Icons</span>
+            <a class="nav-link" href="index.html">
+                <i class="mdi mdi-grid-large menu-icon"></i>
+                <span class="menu-title">Onboarding Request</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#vendors" aria-expanded="false" aria-controls="auth">
+                <i class="menu-icon mdi mdi-account-circle-outline"></i>
+                <span class="menu-title">Seller</span>
                 <i class="menu-arrow"></i>
             </a>
-            <div class="collapse" id="icons">
+            <div class="collapse" id="vendors">
                 <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"> <a class="nav-link" href="pages/icons/font-awesome.html">Font Awesome</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="pages/samples/blank-page.html"> Seller List </a></li>
+                    <li class="nav-item"> <a class="nav-link" href="pages/samples/error-404.html"> Add new Seller </a></li>
                 </ul>
             </div>
         </li>
+       
+        <li class="nav-item nav-category">Customer management</li>
         <li class="nav-item">
-            <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+            <a class="nav-link" data-bs-toggle="collapse" href="#customers" aria-expanded="false" aria-controls="auth">
                 <i class="menu-icon mdi mdi-account-circle-outline"></i>
-                <span class="menu-title">User Pages</span>
+                <span class="menu-title">Customers</span>
                 <i class="menu-arrow"></i>
             </a>
-            <div class="collapse" id="auth">
+            <div class="collapse" id="customers">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a class="nav-link" href="pages/samples/blank-page.html"> Blank Page </a></li>
+                    <li class="nav-item"> <a class="nav-link" href="pages/samples/error-404.html"> 404 </a></li>
+                    <li class="nav-item"> <a class="nav-link" href="pages/samples/error-500.html"> 500 </a></li>
+                    <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
+                    <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
+                </ul>
+            </div>
+        </li>
+
+        <li class="nav-item nav-category">System management</li>
+        <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#settings" aria-expanded="false" aria-controls="auth">
+                <i class="menu-icon mdi mdi-account-circle-outline"></i>
+                <span class="menu-title">Setting management</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="settings">
                 <ul class="nav flex-column sub-menu">
                     <li class="nav-item"> <a class="nav-link" href="pages/samples/blank-page.html"> Blank Page </a></li>
                     <li class="nav-item"> <a class="nav-link" href="pages/samples/error-404.html"> 404 </a></li>
